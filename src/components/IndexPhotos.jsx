0a711e36cad69c132e9610442537e9167f3334f6
k-0107 <k.tkm0107@gmail.com>
2024-02-07 10:56:04 +0900

    [削除機能]アクセス権限拒否で実行されない

--- src/components/IndexPhotos.jsx
+++ src/components/IndexPhotos.jsx
@@ -3,18 +3,31 @@ import React, { useState, useEffect } from "react";
 function IndexPhotos() {
   const [photos, setPhotos] = useState([]);
 
+  const fetchPhotos = async () => {
+    try {
+      const response = await fetch("/photos");
+      const data = await response.json();
+      setPhotos(data.photos);
+    } catch (error) {
+      console.error("ng", error);
+    }
+  };
   useEffect(() => {
-    const fetchPhotos = async () => {
-      try {
-        const response = await fetch("/photos");
-        const data = await response.json();
-        setPhotos(data.photos);
-      } catch (error) {
-        console.error("ng", error);
-      }
-    };
     fetchPhotos();
   }, []);
+
+  const handleDeletePhoto = async (id) => {
+    try {
+      const response = await fetch(`/photo/${id}`, {
+        method: "DELETE",
+      });
+      const data = await response.json();
+      console.log(data);
+      fetchPhotos();
+    } catch (error) {
+      console.error("Error deleting photo:", error);
+    }
+  };
   return (
     <div>
       <h2>写真一覧</h2>
@@ -22,6 +35,7 @@ function IndexPhotos() {
         {photos.map((photo) => (
           <li key={photo._id}>
             <img src={photo.imageUrl} />
+            <button onClick={() => handleDeletePhoto(photo._id)}>削除</button>
           </li>
         ))}
       </ul>
